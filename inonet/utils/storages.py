054471a742d1719d96ea19a7a6fbcac22b4b401d
Soheil Paidar <soheilpaidar@gmail.com>
2021-09-14 16:29:47 +0430

    static

--- inonet/utils/storages.py
+++ inonet/utils/storages.py
@@ -4,6 +4,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
+    querystring_auth=False
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
