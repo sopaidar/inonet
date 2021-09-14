a52ed87283da472a945951ff38894113faa4c2ce
Soheil Paidar <soheilpaidar@gmail.com>
2021-09-14 15:23:55 +0430

    static

--- config/settings/production.py
+++ config/settings/production.py
@@ -86,7 +86,7 @@ aws_s3_static = AWS_S3_CUSTOM_DOMAIN or f"s3.ir-thr-at1.arvanstorage.com/{AWS_ST
 # STATIC
 # ------------------------
 STATICFILES_STORAGE = "inonet.utils.storages.StaticRootS3Boto3Storage"
-STATIC_URL = f"https://{aws_s3_static}/STATIC/"
+STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "inonet.utils.storages.MediaRootS3Boto3Storage"
