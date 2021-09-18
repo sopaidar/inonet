787dac7a90a5f29377e7135524fbb8e4f83b90a6
Soheil Paidar <soheilpaidar@gmail.com>
2021-09-18 18:30:20 +0430

    meta tag

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["narengi.net", "trustseal.enamad.ir"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["narengi.net"])
+X_FRAME_OPTIONS = 'ALLOW-FROM https://trustseal.enamad.ir/'
 
 # DATABASES
 # ------------------------------------------------------------------------------
