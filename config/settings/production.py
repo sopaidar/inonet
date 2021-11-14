0189ee962879108b64d0feb5e9969747413a0b53
Soheil <soheil.cto@gmail.com>
2021-11-14 17:00:41 +0330

    mail

--- config/settings/production.py
+++ config/settings/production.py
@@ -187,13 +187,13 @@ LOGGING = {
 # ------------------------------------------------------------------------------
 ##### Email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="mail.narengi <no-reply@mail.narengi.net>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Narengi <no-reply@mail.narengi.net>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[mail.narengi.net]"
+    "DJANGO_EMAIL_SUBJECT_PREFIX", default=""
 )
 
 EMAIL_HOST = "mail.mail.narengi.net"
